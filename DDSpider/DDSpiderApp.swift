ac8348b407f4f41024e547e3236b3cb47927a3b4
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-04-30 22:14:09 +0900

    Initial Commit

---
+++ DDSpider/DDSpiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDSpiderApp.swift
+//  DDSpider
+//
+//  Created by joe on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct DDSpiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
