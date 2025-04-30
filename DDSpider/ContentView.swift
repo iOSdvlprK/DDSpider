a54e2a5978042121c8ad2f74fd9c47377dd706f2
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-04-30 22:49:10 +0900

    explore Canvas

--- DDSpider/ContentView.swift
+++ DDSpider/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    let dots: [CGPoint]
+    let n = 200
+    
+    init() {
+        self.dots = (0..<n)
+            .map { _ in
+                CGPoint(
+                    x: CGFloat.random(in: 0...1),
+                    y: CGFloat.random(in: 0...1)
+                )
+            }
+    }
+    
+    let circleRadius = 75.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.ignoresSafeArea()
+            
+            Canvas { context, size in
+                let center = CGPoint(x: size.width / 2, y: size.height / 2)
+                let circleRect = CGRect(
+                    x: center.x - circleRadius,
+                    y: center.y - circleRadius,
+                    width: circleRadius * 2,
+                    height: circleRadius * 2
+                )
+                
+                context.fill(Path(ellipseIn: circleRect), with: .color(.blue))
+            }
         }
-        .padding()
     }
 }
 
