3286e16e33897a0361d80863f376ab11d725d2a2
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-05-01 23:20:58 +0900

    generating random points on Canvas

--- DDSpider/ContentView.swift
+++ DDSpider/ContentView.swift
@@ -21,7 +21,9 @@ struct ContentView: View {
             }
     }
     
-    let circleRadius = 75.0
+    let dotRadius = 2.5
+    @State private var dragLocation: CGPoint = .zero
+    let radiusOfInfluence = 0.3 // in values between 0 and 1
     
     var body: some View {
         ZStack {
@@ -30,14 +32,50 @@ struct ContentView: View {
             Canvas { context, size in
                 let center = CGPoint(x: size.width / 2, y: size.height / 2)
                 let circleRect = CGRect(
-                    x: center.x - circleRadius,
-                    y: center.y - circleRadius,
-                    width: circleRadius * 2,
-                    height: circleRadius * 2
+                    x: center.x - dotRadius,
+                    y: center.y - dotRadius,
+                    width: dotRadius * 2,
+                    height: dotRadius * 2
                 )
                 
                 context.fill(Path(ellipseIn: circleRect), with: .color(.blue))
+                
+                drawDotsAndLines(
+                    context: context,
+                    size: size,
+                    dots: dots,
+                    dotRadius: dotRadius,
+                    dragLocation: dragLocation,
+                    radiusOfInfluence: radiusOfInfluence
+                )
             }
+        }
+    }
+    
+    func drawDotsAndLines(
+        context: GraphicsContext,
+        size: CGSize,
+        dots: [CGPoint],
+        dotRadius: CGFloat,
+        dragLocation: CGPoint,
+        radiusOfInfluence: CGFloat
+    ) {
+        for dot in dots {
+            let actualX = dot.x * size.width
+            let actualY = dot.y * size.height
+            
+            let dim = dotRadius * 2
+            
+            let circle = Path(
+                ellipseIn: CGRect(
+                    x: actualX - dotRadius,
+                    y: actualY - dotRadius,
+                    width: dim,
+                    height: dim
+                )
+            )
+            
+            context.fill(circle, with: .color(.blue))
         }
     }
 }
